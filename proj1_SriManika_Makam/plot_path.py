9845de3b8e8b9d780458381b4cc2d7671e8cebd5
manikamakam <srimanika1998@gmail.com>
2020-02-17 23:40:32 -0500

    Adding the action functions

--- proj1_SriManika_Makam/plot_path.py
+++ proj1_SriManika_Makam/plot_path.py
@@ -1,42 +1,16 @@
 import numpy as np
 
-# def print_matrix(state):
-#     counter = 0
-#     for row in range(0, len(state), 3):
-#         if counter == 0 :
-#             print("-------------")
-#         for element in range(counter, len(state), 3):
-#             if element <= counter:
-#                 print("|", end=" ")
-#             print(int(state[element]), "|", end=" ")
-#         counter = counter +1
-#         print("\n-------------")
 
-# fname = 'nodePath.txt'
-# data = np.loadtxt(fname)
-# if len(data[1]) is not 9:
-#     print("Format of the text file is incorrect, retry ")
-# else:
-#     for i in range(0, len(data)):
-#         if i == 0:
-#             print("Start Node")
-#         elif i == len(data)-1:
-#             print("Achieved Goal Node")
-#         else:
-#             print("Step ",i)
-#         print_matrix(data[i])
-#         print()
-#         print()
-
-
-
-
-
+class Puzzle:
+    def __init__(self, node_index, node_data, parent):
+        self.node_index = node_index
+        self.node_data = node_data
+        self.parent = parent
 
 def BlankTileLocation(node):
     i,j = np.where(node == 0)
     return i,j
-    
+
 
 def ActionMoveLeft(node):
     i, j = BlankTileLocation(node)
@@ -85,6 +59,47 @@ def ActionMoveDown(node):
         temp_arr[i + 1, j] = 0
         return temp_arr
 
+def MoveTile(action, node):
+    if action == 'up':
+        return ActionMoveUp(node)
+    if action == 'down':
+        return ActionMoveDown(node)
+    if action == 'left':
+        return ActionMoveLeft(node)
+    if action == 'right':
+        return ActionMoveRight(node)
+    else:
+        return None
+
+def BFS(x,goal_node):
+    actions = ["down", "up", "left", "right"]
+    node = [x]
+    nodes_list = []
+    visited_nodes = []
+    nodes_list.append(node[0].node_data.tolist())  
+    visited_nodes.append(x)
+    index = 0
+
+    while node:
+        current_node = node.pop(0)
+        if current_node.node_data.tolist() == goal_node.tolist():
+            print("Goal reached")
+            return current_node, nodes_list, visited_nodes
+
+        for move in actions:
+            temp = MoveTile(move, current_node.node_data)
+            if temp is not None:
+                index += 1
+                child_node = Puzzle(index, np.array(temp), current_node)
+
+                if child_node.node_data.tolist() not in nodes_list: 
+                    node.append(child_node)
+                    nodes_list.append(child_node.node_data.tolist())
+                    visited_nodes.append(child_node)
+                    if child_node.node_data.tolist() == goal_node.tolist():
+                        print("Goal_reached")
+                        return child_node, nodes_list, visited_nodes
+    return None
 
 
 
@@ -104,6 +119,15 @@ print("Input node: " , input_node)
 
 # posiiton = BlankTileLocation(input_node)
 # print(position)
+
+goal_node = np.array([[1,2,3],[4,5,6],[7,8,0]])
+print("Goal node:", goal_node)
+
+
+obj = Puzzle(0, input_node, None)
+
+goal, nodes_list, visited_nodes = BFS(obj, goal_node)
+
 
 
 
